dc3f427ac7fcf97148458fb294e04615c64bac2c
Alex Baucom <alexbaucom17@gmail.com>
2025-10-26 15:47:07 -0700

    Update last modified hook to add cuttoff date

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -3,12 +3,18 @@
 # Check for changed posts
 
 Jekyll::Hooks.register :posts, :post_init do |post|
+  # Define the cutoff date (ISO format so Ruby Time can parse it)
+  cutoff_date = Time.parse("2025-10-28T00:00:00-0700")
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  commit_num = `git rev-list --count HEAD "#{ post.path }"`.to_i
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+  if commit_num > 1
+    lastmod_str = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`.strip
+    lastmod_time = Time.parse(lastmod_str) rescue nil
+
+    # Only apply if the lastmod date is AFTER the cutoff
+    if lastmod_time && lastmod_time > cutoff_date
+      post.data['last_modified_at'] = lastmod_time.iso8601
+    end
   end
-
 end
